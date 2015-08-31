2b95463bba06d74ccbf5e78d3168cb4c1d853d02
Han <yanhan02008@gmail.com>
2015-08-30 22:19:18 -0500

    fix for swift 1.2

--- cowhacker/src/main.swift
+++ cowhacker/src/main.swift
@@ -13,7 +13,7 @@ func parseBasePath (file: NSFileHandle) -> String {
     let pathMaxLen = 512
     let readBuffer = UnsafeMutablePointer<UInt8>.alloc(pathMaxLen)
     data.getBytes(readBuffer, range: NSMakeRange(pathOffset, pathMaxLen))
-    
+
     let buf = UnsafeMutablePointer<CChar>(readBuffer)
     buf[pathMaxLen - 1] = 0
     var str = ""
@@ -24,35 +24,33 @@ func parseBasePath (file: NSFileHandle) -> String {
     return str
 }
 
-enum UpdateError : ErrorType {
-    case TooLong
-}
-
-func updateBasePath (file: NSFileHandle, newPath: String) throws {
-    if newPath.characters.count > 256 {
-        print("Path too long")
-        throw(UpdateError.TooLong)
+func updateBasePath (file: NSFileHandle, newPath: String) -> Int {
+    if count(newPath) > 256 {
+        println("Path too long")
+        return 1
     }
     // Write length byte
     let lengthByteOffset: UInt64 = 0x13
     file.seekToFileOffset(lengthByteOffset)
-    let lengthByte = NSData(bytes: [ UInt8(newPath.characters.count) ], length: 1)
+    let lengthByte = NSData(bytes: [ UInt8(count(newPath)) ], length: 1)
     file.writeData(lengthByte)
-    
+
     // Write path
     let pathOffset: UInt64 = 0xA0 + 24
     file.seekToFileOffset(pathOffset)
     let strData = NSMutableData(data: newPath.dataUsingEncoding(NSUTF8StringEncoding)!)
     strData.appendBytes([ UInt8(0) ], length: 1)
     file.writeData(strData)
+
+    return 0
 }
 
 // Entry point
 
 if Process.arguments.count < 3 {
-    print("Usage: \(Process.arguments[0]) [patch|revert] qcow_path [orig_path]")
-    print("   1) '\(Process.arguments[0]) patch qcow_path' outputs original path to backing image")
-    print("   2) '\(Process.arguments[0]) revert qcow_path orig_path' sets path to backing image back to orig_path")
+    println("Usage: \(Process.arguments[0]) [patch|revert] qcow_path [orig_path]")
+    println("   1) '\(Process.arguments[0]) patch qcow_path' outputs original path to backing image")
+    println("   2) '\(Process.arguments[0]) revert qcow_path orig_path' sets path to backing image back to orig_path")
     exit(1)
 }
 
@@ -64,46 +62,47 @@ if let modeStr = String.fromCString(Process.arguments[1]) {
     } else if modeStr == "revert" {
         mode = Mode.Revert
     } else {
-        print("Invalid mode")
+        println("Invalid mode")
         exit(1)
     }
 } else {
-    print("Invalid mode")
+    println("Invalid mode")
     exit(1)
 }
 
 func main () -> Int32 {
     if let path = String.fromCString(Process.arguments[2]) {
         if let file = NSFileHandle(forUpdatingAtPath: path) {
-            defer {
-                file.closeFile()
-            }
-            do {
-                if mode == Mode.Patch {
-                    print(parseBasePath(file))
-                    try updateBasePath(file, newPath: "C:\\ece391\\devel\\ece391.qcow")
+            // defer {
+            //     file.closeFile()
+            // }
+            let error: Int
+            if mode == Mode.Patch {
+                println(parseBasePath(file))
+                error = updateBasePath(file, "C:\\ece391\\devel\\ece391.qcow")
+            } else {
+                if let newPath = Process.arguments.count >= 4 ? String.fromCString(Process.arguments[3]) : nil {
+                    error = updateBasePath(file, newPath)
                 } else {
-                    if let newPath = Process.arguments.count >= 4 ? String.fromCString(Process.arguments[3]) : nil {
-                        try updateBasePath(file, newPath: newPath)
-                    } else {
-                        print("Please specify new backing image path")
-                        return 1
-                    }
+                    println("Please specify new backing image path")
+                    file.closeFile()
+                    return 1
                 }
-            } catch {
-                print("Error occurred")
+            }
+            if error != 0 {
+                println("Error occurred")
+                file.closeFile()
                 return 1
             }
         } else {
-            print("Cannot open file")
+            println("Cannot open file")
             return 1
         }
     } else {
-        print("Invalid path")
+        println("Invalid path")
         return 1
     }
     return 0
 }
 
 exit(main())
-
