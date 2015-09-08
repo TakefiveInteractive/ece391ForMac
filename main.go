eb3a525c988519f2dc9999fe550e4b20111f0915
Yifei Teng <yifei@skycatch.com>
2015-09-07 23:28:31 -0500

    Determine installation location

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
     "fmt"
     "os"
+    "path/filepath"
     "github.com/Tedko/ece391ForMac/wget"
     "github.com/Tedko/ece391ForMac/brew"
     "github.com/Tedko/ece391ForMac/util"
+    "github.com/mitchellh/go-homedir"
 )
 
 func credits() {
@@ -19,8 +21,21 @@ Welcome to ece391 setup for OS X/Linux
 `)
 }
 
-func install() {
+func install() error {
     credits()
+
+    var homePath string
+    var err error
+    if homePath, err = homedir.Dir(); err != nil { return err }
+    basePath := filepath.Join(homePath, "ece391ForMac")
+    if os.Getenv("ECE391_INSTALL_PATH") != "" {
+        if result, err := homedir.Expand(os.Getenv("ECE391_INSTALL_PATH")); err != nil {
+            return err
+        } else {
+            if basePath, err = filepath.Abs(result); err != nil { return err }
+        }
+    }
+    fmt.Println("Installing to " + basePath)
     fmt.Println("Pre-flight checks...")
     out := func (str string) { fmt.Println("\t" + str) }
     if !util.BinExists("unison") {
@@ -30,10 +45,13 @@ func install() {
     } else {
         out("Found Unison")
     }
+    if _, err := os.Stat(""); os.IsNotExist(err) {
+
+    }
+    return nil
 }
 
 func main() {
     install()
     wget.WgetCli(os.Args)
-    os.Exit(1)
 }
